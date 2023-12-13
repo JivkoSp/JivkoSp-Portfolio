d824b892ca20fbdc7abe5eeafb1bf7283824c2cb
JivkoSp <atjspasov@gmail.com>
2023-12-13 19:27:36 +0300

    Modified app/components/HeroSection.jsx

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -5,52 +5,53 @@ import { TypeAnimation } from "react-type-animation";
 
 const HeroSection = () => {
   return (
-    <section className="grid grid-cols-1 lg:grid-cols-12 my-4">
-      <div className="col-span-5 place-self-center mt-4 mb-5 lg:mt-0">
-        <div className="lg:w-[400px] lg:h-[400px] w-[250px] h-[250px] rounded-full relative">
-          <Image
-            src="/images/me.png"
-            alt="hero image"
-            className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 rounded-3xl"
-            width={320}
-            height={320}
-          />
-        </div>
-      </div>
-      <div className="col-span-7 place-self-center place-items-center grid lg:place-items-start mt-2">
-        <h1 className="text-white max-w-2xl mb-4 lg:text-6xl text-4xl font-extrabold">
-          <span className="text-transparent font-mono text-6xl bg-clip-text bg-gradient-to-r from-purple-300 via-purple-400 to-purple-700">
-            Hello, I'm{" "}
-          </span>{" "}
-          <br></br>
-          <TypeAnimation className="text-purple-700 font-mono"
-            sequence={[
-              "Jivko Spasov",
-              1000,
-              "Web Developer",
-              1000,
-            ]}
-            wrapper="span"
-            speed={50}
-            repeat={Infinity}
-          />
-        </h1>
-        <p className="text-[#ADB7BE] mb-6 text-base sm:text-lg lg:text-xl font-mono">
-          I like <strong className="text-blue-300">solving</strong>  real-life problems with code. 
-          I am currently finishing my master's degree in <strong className="text-blue-400">Cybersecurity</strong>.
-        </p>
-        <div>
-          <button className="bg-gradient-to-br bg-purple-950 hover:bg-purple-800 text-white px-6 py-3 rounded-full mr-4 font-mono">
-            Hire Me
-          </button>
-          <button className="m-4 bg-gradient-to-br from-blue-500 via-purple-600 to-purple-800 px-0.5 py-0.5  text-white rounded-full">
-            <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2 font-mono">
-              Download CV
+    <section>
+      <div className="grid grid-cols-1 sm:grid-cols-12">
+        <div className="col-span-7 place-self-center text-center sm:text-left">
+          <h1 className="text-purple-600 font-mono mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
+            <span className="text-transparent font-mono bg-clip-text bg-gradient-to-r from-purple-300 to-purple-500">
+              Hello, I'm{" "}
             </span>
-          </button>
+            <br></br>
+            <TypeAnimation
+              sequence={[
+                "Jivko Spasov",
+                1000,
+                "Web Developer",
+                1000,
+              ]}
+              wrapper="span"
+              speed={50}
+              repeat={Infinity}
+            />
+          </h1>
+          <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
+            voluptatum.
+          </p>
+          <div>
+            <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white">
+              Hire Me
+            </button>
+            <button className="px-0.5 py-0.5 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-400 via-blue-600 to-stone-800 hover:text-black hover:from-black text-white mt-3">
+              <span className="block bg-[#121212] hover:bg-stone-200 rounded-full px-5 py-2">
+                Download CV
+              </span>
+            </button>
+          </div>
+        </div>
+        <div className="col-span-5 place-self-center mt-4 lg:mt-0">
+          <div className="w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+            <Image
+              src="/images/cartoon-me.png"
+              alt="hero image"
+              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 rounded-2xl"
+              width={300}
+              height={300}
+            />
+          </div>
         </div>
       </div>
-      
     </section>
   );
 };
