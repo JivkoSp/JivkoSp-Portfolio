429a5979d02a93cc1b9c6314f924186e0d5a4a1a
JivkoSp <atjspasov@gmail.com>
2023-12-13 21:39:03 +0300

    Updated app/components/AboutSection.jsx

--- app/components/AboutSection.jsx
+++ app/components/AboutSection.jsx
@@ -63,12 +63,9 @@ const AboutSection = () => {
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-purple-200 mb-4 font-mono">About Me</h2>
           <p className="text-white texxt-base md:text-lg font-mono">
-            I am a full stack web developer with a passion for creating
+            I am a full stack web developer with a passion for creating reliable,
             interactive and responsive web applications. I have experience
-            working with C#, .NET, JavaScript, PostgreSQL, Elasticsearch, gRPC and Git. 
-            I have also recently worked on <strong>microservice</strong> application that showcases my 
-            understanding of the mentioned technologies. I am a quick learner and I am always
-            looking to expand my knowledge and skill set.
+            working with C#, .NET, JavaScript, PostgreSQL, Elasticsearch, gRPC and Git. I am always looking to expand my knowledge and skill set.
           </p>
           <div className="flex flex-row justify-start mt-8">
             <TabButton
