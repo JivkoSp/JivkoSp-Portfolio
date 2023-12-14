6d6b40c48760a3963c16ca0ffa522b2716183881
JivkoSp <atjspasov@gmail.com>
2023-12-14 20:14:06 +0300

    Added route for email send: app/api/send/route.js

---
+++ app/api/send/route.js
@@ -0,0 +1,23 @@
+import { NextResponse } from "next/server";
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export async function POST() {
+  try {
+    const data = await resend.emails.send({
+      from: "Jivko Spasov <atjspasov@gmail.com>",
+      to: ["atjspasov@gmail.com"],
+      subject: "Hello world",
+      react: (
+        <>
+          <p>Email Body</p>
+        </>
+      ),
+    });
+
+    return NextResponse.json(data);
+  } catch (error) {
+    return NextResponse.json({ error });
+  }
+}
