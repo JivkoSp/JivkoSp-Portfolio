ab4a00862ed2b4638a62ebba39b2fb5af5fa97b3
JivkoSp <atjspasov@gmail.com>
2023-12-14 21:15:52 +0300

    Updated app/components/HeroSection.jsx

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -8,7 +8,7 @@ const HeroSection = () => {
     <section>
       <div className="grid grid-cols-1 sm:grid-cols-12">
         <div className="col-span-7 place-self-center text-center sm:text-left">
-          <h1 className="text-purple-600 font-mono mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
+          <h1 className="text-purple-600 font-mono mb-4 text-4xl sm:text-4xl md:text-5xl lg:text-6xl font-extrabold">
             <span className="text-transparent font-mono bg-clip-text bg-gradient-to-r from-purple-300 to-purple-500">
               Hello, I&apos;m{" "}
             </span>
@@ -17,7 +17,7 @@ const HeroSection = () => {
               sequence={[
                 "Jivko Spasov",
                 1000,
-                "Web Developer",
+                "Developer",
                 1000,
               ]}
               wrapper="span"
