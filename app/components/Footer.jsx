e212882ba0bbcd5822965840f6685eb82a88e08a
JivkoSp <atjspasov@gmail.com>
2023-12-15 17:37:48 +0300

    Updated app/components/Footer.jsx

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -2,8 +2,8 @@ const Footer = () => {
     return (
       <footer className="footer border border-t-[#33353F] border-l-transparent border-r-transparent text-white">
         <div className="container p-12 flex flex-col text-center m-auto">
-          <span className="text-muted">LOGO</span>
-          <p className="text-slate-400"> &copy; 2024 jivkospasov. All Rights Reserved.</p>
+          <span className="text-muted text-slate-400 ">Spasov</span>
+          <p className="text-slate-400"> &copy; 2024 All Rights Reserved.</p>
         </div>
       </footer>
     );
